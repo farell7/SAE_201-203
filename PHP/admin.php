42de6a929913832defe652176210720ed9738f2e
Alexouuuu <alexchisiu@gmail.com>
2025-05-25 11:57:35 +0200

    ajout de pages pour le navbar

--- PHP/admin.php
+++ PHP/admin.php
@@ -10,8 +10,6 @@ $username = isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username'
     <title>Admin - Université Eiffel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
